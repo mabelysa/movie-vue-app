980e6f37709e32585b8d8c6cfd78fc9408ec6a7f
Mabelys Arias <MabelysA@gmail.com>
2021-07-22 00:43:52 -0400

    adds authentication

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <button v-on:click="createMovie()">Create Movie</button>
+    <div v-for="movie in movies" :key="movie.id">
+      <h2>Title: {{ movie.title }}</h2>
+      <h1>Year: {{ movie.year }}</h1>
+      <p>Plot: {{ movie.plot }}</p>
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to My Movie App!",
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movie").then((response) => {
+        this.movies = response.data;
+        console.log("All Movies:", this.movies);
+      });
+    },
+    createMovie: function () {
+      console.log("Create a new movie!");
+
+      var params = {
+        title: "Spice World",
+        year: "1997",
+        plot: "Spice Girls Movie",
+        director: "Bob Spiers",
+      };
+      axios
+        .post("http://localhost:3000/movie", params)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.movies.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
   },
 };
 </script>
