dc8ca7556da1354f560d75990e68251b909d92a3
Mabelys Arias <MabelysA@gmail.com>
2021-07-23 11:42:52 -0400

    adds restful routes

---
+++ src/views/MoviesIndex.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="movies-index">
+    <div v-for="movie in movies" :key="movie.id">
+      <router-link v-bind:to="`/movies/${movie.id}`">
+        <h2>Title: {{ movie.title }}</h2>
+      </router-link>
+      <p>Year: {{ movie.chef }}</p>
+      <p>Plot: {{ movie.plot }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Here are all movies!",
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        this.movies = response.data;
+        console.log("All movies:", this.recipes);
+      });
+    },
+  },
+};
+</script>
